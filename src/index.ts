5e549d12e81d927f18fff39b130d2f649100666b
beekocha <lisabyrdina@gmail.com>
2019-11-19 21:37:19 +0400

    Adding new class - Map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { User } from './User';
 import { Company } from './Company';
+import { Map } from './Map';
 
 const user = new User;
 const company = new Company;
+new Map('map');
 
-console.log(user);
-console.log(company);
+// console.log(user);
+// console.log(company);
